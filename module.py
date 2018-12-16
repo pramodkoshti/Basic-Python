7d771b6d3fb3bbb568bff2c5dc891d769048555e
Pramod <pramodskohti7389@gmail.com>
2018-12-17 00:49:47 +0530

    module.py added

---
+++ module.py
@@ -0,0 +1,11 @@
+# module in python is collection of variales and functiosn 
+# we will import this module in our other files
+
+def display(name):
+	print("Welcome to module " + name)
+
+
+module_dict = {
+	"name" : "Pramod"
+	"age" : 15
+}	
